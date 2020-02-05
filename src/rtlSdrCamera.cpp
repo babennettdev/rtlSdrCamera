b5235d3ea100fb9bd5fbd1c6579d444ec2a2327f
Blake Bennett <babennettdev@gmail.com>
2020-02-04 21:11:06 -0500

    Quick sanity check to ensure OpenCV is linked properly, code in rtlSdrCamera.cpp will be removed in future commit.

--- src/rtlSdrCamera.cpp
+++ src/rtlSdrCamera.cpp
@@ -3,19 +3,25 @@
 //includes
 #include <iostream>
 #include <string>
+#include <opencv2/opencv.hpp>
 //#include "../build/rtlSdrCameraConfig.h"
 #include "rtlSdrCameraConfig.h"
 
 int main(int argc, char* argv[])
 {
-	if (argc < 2){
+	if (argc < 3){
 	std::cout << argv[0] << " Version " << RTLSDRCAMERA_VERSION_MAJOR << "."
 		<< RTLSDRCAMERA_VERSION_MINOR << "." << RTLSDRCAMERA_VERSION_PATCH
 		<< std::endl;
-	std::cout << "Usage: " << argv[0] << "Tune frequency" << std::endl;
+	std::cout << "Usage: " << argv[0] << "Tune frequency " << "<Test_Image_Path>" << std::endl;
 	return 1;
 	}
 
+	cv::Mat testImage;
+	testImage = cv::imread( argv[2], 1);
+	cv::namedWindow("Test Image", cv::WINDOW_AUTOSIZE );
+	cv::imshow("Test Image", testImage);
+	cv::waitKey(0);
 	//TODO main function processing
 
 	return 0;
