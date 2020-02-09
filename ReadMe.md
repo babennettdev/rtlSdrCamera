f10f222800706746f91983e7ecbbe96e9e91ae20
Blake Bennett <babennettdev@gmail.com>
2020-02-09 13:23:32 -0500

    Checking rtl-sdr is linked properly. Added rtl-sdr test in rtlSdrCamera.cpp that should eventually be moved to a test directory.

--- ReadMe.md
+++ ReadMe.md
@@ -1,1 +1,34 @@
 # rtlSdrCamera
+
+### Dependencies
+
+#### libusb
+##### Install with package manager
+	Install with: "sudo apt-get install libusb1.0-0-dev"
+
+#### rtl-sdr
+
+##### Clone github repository and install rtl-sdr library
+	Clone github repository: "git clone https://github.com/osmocom/rtl-sdr"
+	cd /path/to/rtl-sdr
+	mkdir build/
+	cd build/
+	cmake ../ -DINSTALL_UDEV_RULES=ON -DDETACH_KERNEL_DRIVER=ON
+	make
+	sudo make install
+
+##### Set rules to use rtl-sdr dongle 
+	cd ~/path/to/rtl-sdr
+	sudo cp rtl-sdr.rules /etc/udev/rules.d/
+	sudo udevadm control --reload-rules
+	sudo udevadm trigger
+
+#### Build Project
+
+	cd /path/to/rtlSdrCamera
+	mkdir build/
+	cd build/
+	cmake ../ 
+	make
+	cd src
+	./rtlSdrCamera [device_id] [frequency] [/path/to/testImage]
--- src/rtlSdrCamera.cpp
+++ src/rtlSdrCamera.cpp
@@ -4,24 +4,49 @@
 #include <iostream>
 #include <string>
 #include <opencv2/opencv.hpp>
-//#include "../build/rtlSdrCameraConfig.h"
+#include <rtl-sdr.h>
+
 #include "rtlSdrCameraConfig.h"
+
+static rtlsdr_dev_t *dev = NULL;
 
 int main(int argc, char* argv[])
 {
-	if (argc < 3){
+	if (argc < 4){
 	std::cout << argv[0] << " Version " << RTLSDRCAMERA_VERSION_MAJOR << "."
 		<< RTLSDRCAMERA_VERSION_MINOR << "." << RTLSDRCAMERA_VERSION_PATCH
 		<< std::endl;
-	std::cout << "Usage: " << argv[0] << "Tune frequency " << "<Test_Image_Path>" << std::endl;
+	std::cout << "Usage: " << argv[0]  << "Device ID (default 0)" << "Tune frequency " << "<Test_Image_Path>" << std::endl;
 	return 1;
 	}
 
+	// Simple OpenCV test to check if library is working... eventually move to a test directory?
 	cv::Mat testImage;
-	testImage = cv::imread( argv[2], 1);
+	testImage = cv::imread( argv[3], 1);
 	cv::namedWindow("Test Image", cv::WINDOW_AUTOSIZE );
 	cv::imshow("Test Image", testImage);
 	cv::waitKey(0);
+
+	// Simple rtl-sdr test to check if library is working... eventually move to a test directory?
+	int r;
+	int dev_index;
+	//static rtlsdr_dev_t *dev = NULL;
+
+	dev_index = atoi(argv[1]);
+	if (dev_index < 0 ){
+		exit(1);
+	}
+
+
+	r = rtlsdr_open(&dev, (uint32_t)dev_index);
+	if ( r < 0 ){
+		std::cout << "Failed to open rtlsdr " << dev_index << std::endl;
+	}
+	
+	std::cout << "RTLSDR IS OPEN!" << std::endl;
+
+	rtlsdr_close(dev);
+
 	//TODO main function processing
 
 	return 0;
